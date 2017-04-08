362344079f7124a3f66b00b084be3fb58f0f2c9e
William Woodruff <william@tuffbizz.com>
2017-04-08 06:48:16 -0400

    lib: Implement running and version checks, config path accessor.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
+extern crate rustc_serialize;
+
 mod keybase {
+	use rustc_serialize::json::Json;
 	use std::process::Command;
+	use std::env;
+	use std::path::Path;
 
 	pub fn ensure_running() -> bool {
 		Command::new("keybase")
@@ -18,5 +23,14 @@ mod keybase {
 					.stdout;
 
 		return String::from_utf8(raw).expect("invalid output");
+	}
+
+	pub fn config_file() -> String {
+		match env::home_dir() {
+			Some(home) => Path::new(home.as_os_str())
+								.join(".config/keybase/config.json")
+								.to_str().unwrap().to_string(),
+			None => panic!("no home dir")
+		}
 	}
 }
